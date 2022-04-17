4d9644ca66220b34eaa099317c83fd3cde2d44f5
deshaware <deshaware.swapnil@gmail.com>
2022-04-17 15:44:48 -0400

    Response Management

---
+++ src/main/java/com/deshaware/shuttleservice/response/ResponseFailure.java
@@ -0,0 +1,16 @@
+package com.deshaware.shuttleservice.response;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Data;
+
+@Data
+public class ResponseFailure implements Serializable {
+    private HttpStatus httpStatus;
+    private String status="FAILED";
+    private Instant processed_on;
+    private Object reason;
+}
---
+++ src/main/java/com/deshaware/shuttleservice/response/ResponseSuccess.java
@@ -0,0 +1,16 @@
+package com.deshaware.shuttleservice.response;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Data;
+
+@Data
+public class ResponseSuccess implements Serializable {
+    private HttpStatus httpStatus;
+    private String status = "SUCCESS";
+    private Instant processed_on;
+    private Object data;
+}
