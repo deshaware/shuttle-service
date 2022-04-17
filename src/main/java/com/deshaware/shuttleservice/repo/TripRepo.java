db894fc03f9812e83f4f723af9755201b0afc80e
deshaware <deshaware.swapnil@gmail.com>
2022-04-17 15:45:01 -0400

    Implement responses in add Trip

--- src/main/java/com/deshaware/shuttleservice/repo/TripRepo.java
+++ src/main/java/com/deshaware/shuttleservice/repo/TripRepo.java
@@ -1,14 +1,24 @@
 package com.deshaware.shuttleservice.repo;
 
+import java.time.Instant;
+import java.util.List;
+
 // import java.util.Optional;
 
 import com.deshaware.shuttleservice.model.Trip;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TripRepo extends JpaRepository<Trip, String>{
-    
+
+    @Query(value = "SELECT * FROM Trip WHERE shuttle_id = :shuttle_id AND scheduled_on = :scheduled_on AND trip_status <> 'CANCELED'", nativeQuery = true)
+    List<Trip> findDuplicateTrip(
+        @Param("shuttle_id") String shuttle_id, 
+        @Param("scheduled_on") Instant scheduled_on
+    );
     
 }
--- src/main/java/com/deshaware/shuttleservice/service/TripServiceImpl.java
+++ src/main/java/com/deshaware/shuttleservice/service/TripServiceImpl.java
@@ -1,6 +1,4 @@
 package com.deshaware.shuttleservice.service;
-
-import java.lang.StackWalker.Option;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
@@ -12,16 +10,21 @@ import com.deshaware.shuttleservice.model.*;
 import com.deshaware.shuttleservice.repo.ShuttleRepo;
 import com.deshaware.shuttleservice.repo.TripRepo;
 import com.deshaware.shuttleservice.repo.UserRepo;
+import com.deshaware.shuttleservice.response.ResponseFailure;
+import com.deshaware.shuttleservice.response.ResponseSuccess;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Service
 @Transactional
 public class TripServiceImpl implements TripService{
-
+    final static Logger logger = LogManager.getLogger(TripServiceImpl.class);
 
     @Autowired
     TripRepo tripRepo;
@@ -29,29 +32,47 @@ public class TripServiceImpl implements TripService{
     UserRepo userRepo;
     @Autowired
     ShuttleRepo shuttleRepo;
-
     @Override
     public ResponseEntity<?> addTrip(TripRequest tripRequest) {
-        List<Trip> trip = tripRepo.findAll();
-        // if (!trip.isEmpty()) {
-        //     return new ResponseEntity<>("Trip already exist", HttpStatus.BAD_REQUEST);
-        // }
-        Optional<User> driver = userRepo.findByEmail(tripRequest.getDriver_id().toLowerCase());
-        if (driver.equals(null)) {
-            return new ResponseEntity<>("Driver does not exist", HttpStatus.BAD_REQUEST);
+        try {
+            Optional<User> driver = userRepo.findByEmail(tripRequest.getDriver_id().toLowerCase());
+            if (driver.equals(null)) {
+                return new ResponseEntity<>("Driver does not exist", HttpStatus.BAD_REQUEST);
+            }
+            Optional<Shuttle> shuttle = shuttleRepo.findById(tripRequest.getShuttle_id().toLowerCase());
+            if (shuttle.isEmpty()) {
+                return new ResponseEntity<>("Shuttle does not exist", HttpStatus.BAD_REQUEST);
+            }
+            // logic to prevent duplicate trips
+            List<Trip> trip = tripRepo.findDuplicateTrip(tripRequest.getShuttle_id(), tripRequest.getScheduled_on());
+            if (!trip.isEmpty()) {
+                return new ResponseEntity<ResponseFailure>(new ResponseFailure(){{
+                    setReason("The trip already exist with shuttle " + tripRequest.getShuttle_id() + " and scheduled on " + tripRequest.getScheduled_on());
+                    setHttpStatus(HttpStatus.BAD_REQUEST);
+                    setProcessed_on(Instant.now());
+                }}, HttpStatus.BAD_REQUEST);
+            }
+
+            Trip new_trip = new Trip();
+            new_trip.setDriver_id(driver.get()); // different
+            new_trip.setShuttle_id(shuttle.get()); // different way
+            new_trip.setScheduled_on(tripRequest.getScheduled_on());
+            new_trip.setModified(Instant.now());
+            new_trip.setTrip_status(TripStatus.INTIATED);
+            tripRepo.save(new_trip);
+            logger.info("Trip Created");
+            return new ResponseEntity<ResponseSuccess>(new ResponseSuccess(){{
+                setData(new_trip);
+                setHttpStatus(HttpStatus.CREATED);
+                setProcessed_on(Instant.now());
+            }} ,HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<ResponseFailure>(new ResponseFailure(){{
+                setReason("Error while creating a trip " + e.getLocalizedMessage());
+                setHttpStatus(HttpStatus.BAD_REQUEST);
+                setProcessed_on(Instant.now());
+            }}, HttpStatus.BAD_REQUEST);
         }
-        Optional<Shuttle> shuttle = shuttleRepo.findById(tripRequest.getShuttle_id().toLowerCase());
-        if (shuttle.isEmpty()) {
-            return new ResponseEntity<>("Shuttle does not exist", HttpStatus.BAD_REQUEST);
-        }
-        Trip new_trip = new Trip();
-        new_trip.setDriver_id(driver.get()); // different
-        new_trip.setShuttle_id(shuttle.get()); // different way
-        new_trip.setScheduled_on(tripRequest.getScheduled_on());
-        new_trip.setModified(Instant.now());
-        new_trip.setTrip_status(TripStatus.INTIATED);
-        tripRepo.save(new_trip);
-        return new ResponseEntity<>(new_trip,HttpStatus.CREATED);
     }
     
 }
