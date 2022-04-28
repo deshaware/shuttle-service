7bb3f8cb5f8487c9f57281f0c03c117900be131b
deshaware <deshaware.swapnil@gmail.com>
2022-04-28 14:20:49 -0400

    refactor shuttle service

--- src/main/java/com/deshaware/shuttleservice/service/ShuttleService.java
+++ src/main/java/com/deshaware/shuttleservice/service/ShuttleService.java
@@ -2,16 +2,17 @@ package com.deshaware.shuttleservice.service;
 
 
 import com.deshaware.shuttleservice.dto.ShuttleRequest;
+import com.deshaware.shuttleservice.response.Response;
 
 import org.springframework.http.ResponseEntity;
 
 public interface ShuttleService {
 
-    public ResponseEntity<?> addShuttle(ShuttleRequest shuttle);
+    public ResponseEntity<Response> addShuttle(ShuttleRequest shuttle);
 
-    public ResponseEntity<?>  viewAllShuttle();
+    public ResponseEntity<Response>  viewAllShuttle();
 
-    public ResponseEntity<?> deleteShuttle(String shuttle_id);
+    public ResponseEntity<Response> deleteShuttle(String shuttle_id);
 
 	// public Shuttle updateShuttle(Shuttle newShuttle);
     
--- src/main/java/com/deshaware/shuttleservice/service/ShuttleServiceImpl.java
+++ src/main/java/com/deshaware/shuttleservice/service/ShuttleServiceImpl.java
@@ -9,6 +9,7 @@ import javax.transaction.Transactional;
 import com.deshaware.shuttleservice.dto.ShuttleRequest;
 import com.deshaware.shuttleservice.model.Shuttle;
 import com.deshaware.shuttleservice.repo.ShuttleRepo;
+import com.deshaware.shuttleservice.response.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,18 +21,19 @@ import org.apache.logging.log4j.Logger;
 @Service
 @Transactional
 public class ShuttleServiceImpl implements ShuttleService {
+   
     final static Logger logger = LogManager.getLogger(TripServiceImpl.class);
+
     @Autowired
     ShuttleRepo shuttleRepo;
 
     @Override
-    public ResponseEntity<?> addShuttle(ShuttleRequest shuttle) {
+    public ResponseEntity<Response> addShuttle(ShuttleRequest shuttle) {
         try {
+            logger.info("Adding new shuttle");
             Optional<Shuttle> checkShuttle = shuttleRepo.findById(shuttle.getShuttle_id());
             if (checkShuttle.isPresent()) {
-                // throw error
-                // throw new Exception("shuttle with " + shuttle.getShuttle_id() + " is already exist");
-                return new ResponseEntity<>("Already exist", HttpStatus.CONFLICT);
+                throw new Exception("Already exist");
             }
             Shuttle newShuttle = new Shuttle();
             newShuttle.setCapacity(shuttle.getCapacity());
@@ -41,32 +43,68 @@ public class ShuttleServiceImpl implements ShuttleService {
             newShuttle.setEnabled(true);
             newShuttle.setModified(Instant.now());
             shuttleRepo.save(newShuttle);
-            return new ResponseEntity<>("Shuttle Added Succesfully", HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Some error", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return new ResponseEntity<Response>(new Response(){{
+                setData(newShuttle);
+                setMessage("Shuttle Added Succesfully");
+                setStatus("SUCCESS");
+            }} , HttpStatus.CREATED);
+
+        } 
+        catch (Exception e) {
+            logger.error("Error while adding shuttle");
+            return new ResponseEntity<Response>(new Response(){{
+                setMessage("Error while adding shuttle");
+                setStatus("ERROR");
+            }}, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @Override
-    public ResponseEntity<?> viewAllShuttle() {
-        List<Shuttle> shuttleList = shuttleRepo.findAll();
-        if (shuttleList.isEmpty()) {
-            return new ResponseEntity<>("No Shuttles Found", HttpStatus.NOT_FOUND);
+    public ResponseEntity<Response> viewAllShuttle() {
+        try {
+            List<Shuttle> shuttleList = shuttleRepo.findAll();
+            if (shuttleList.isEmpty()) {
+                throw new Exception("No Shuttles Found");
+            }
+            return new ResponseEntity<Response>(new Response(){{
+                setData(shuttleList);
+                setMessage("Shuttle Fetched Succesfully");
+                setStatus("SUCCESS");
+            }} , HttpStatus.ACCEPTED);
+            
+        } catch (Exception e) {
+            logger.error("Error while fetching shuttle");
+            return new ResponseEntity<Response>(new Response(){{
+                setData(e.getMessage());
+                setMessage("Error while fetching shuttle");
+                setStatus("ERROR");
+            }} , HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        System.out.println(shuttleList);
-        // return shuttleList;
-        return new ResponseEntity<>(shuttleList, HttpStatus.ACCEPTED);
         
     }
 
     @Override
-    public ResponseEntity<?> deleteShuttle(String shuttle_id) {
-        Optional<Shuttle> getShuttle = shuttleRepo.findById(shuttle_id);
-        if (!getShuttle.isPresent()) {
-            throw new Exception("No Shuttle Found with id" + shuttle_id);
-        }
-        shuttleRepo.deleteById(shuttle_id);
-        return new ResponseEntity<>("Shuttle Deleted Succesfully",HttpStatus.ACCEPTED);
-    }
+    public ResponseEntity<Response> deleteShuttle(String shuttle_id) {
+        try {
+            Optional<Shuttle> getShuttle = shuttleRepo.findById(shuttle_id);
+            if (!getShuttle.isPresent()) {
+                throw new Exception("No Shuttle Found with id" + shuttle_id);
+            }
+            shuttleRepo.deleteById(shuttle_id);
 
+            return new ResponseEntity<Response>(new Response(){{
+                setMessage("Shuttle Deleted Succesfully");
+                setStatus("SUCCESS");
+            }} , HttpStatus.ACCEPTED);
+            
+        } catch (Exception e) {
+            logger.error("Error while deleting shuttle");
+            return new ResponseEntity<Response>(new Response(){{
+                setData(e.getMessage());
+                setMessage("Error while deleting shuttle");
+                setStatus("ERROR");
+            }} , HttpStatus.INTERNAL_SERVER_ERROR);
+       }
+    }
 }
