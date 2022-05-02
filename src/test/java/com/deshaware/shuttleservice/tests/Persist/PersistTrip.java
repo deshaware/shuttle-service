a94ef61e94a05bc5099900a860440b3638ba314d
deshaware <deshaware.swapnil@gmail.com>
2022-05-02 14:41:18 -0400

    test persist

---
+++ src/test/java/com/deshaware/shuttleservice/tests/Persist/PersistTrip.java
@@ -0,0 +1,13 @@
+package com.deshaware.shuttleservice.tests.Persist;
+
+public class PersistTrip {
+    String trip_id;
+
+    public String getTrip_id() {
+        return trip_id;
+    }
+
+    public void setTrip_id(String trip_id) {
+        this.trip_id = trip_id;
+    }
+}
